cd7cc840d4f2cd7b62f7b26bb4b748c0917281ee
Chi Kei Chan <chikeichan@gmail.com>
2018-10-29 16:57:42 +0000

    Add Sass Extends

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,9 +3,30 @@ import PropTypes from 'prop-types';
 import './header.scss';
 
 export default class Header extends Component {
+  static propTypes = {
+    blockHeight: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    blockHeight: 1234,
+  };
+
   render() {
+    const { blockHeight } = this.props;
+
     return (
-      <div className="header__hi">I am a header</div>
+      <div className="header">
+        <div className="header__title">
+          Handshake Wallet
+        </div>
+        <div className="header__block-height">
+          {`Current Height: ${blockHeight}`}
+        </div>
+        <div className="header__status">
+          Synced
+        </div>
+      </div>
     );
   }
 }
--- app/containers/Window/WindowApp.js
+++ app/containers/Window/WindowApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from '../../components/Header';
+import './window.scss';
 
 @connect(
   state => ({
