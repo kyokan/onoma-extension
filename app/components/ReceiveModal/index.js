bb8db1cf7be60d3f68f466cfdfca2f2f39099c17
Chi Kei Chan <chikeichan@gmail.com>
2018-11-03 17:58:01 +0100

    Send Modal (WIP)

---
+++ app/components/ReceiveModal/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Modal from '../Modal';
+
+export default class ReceiveModal extends Component {
+  render() {
+    return (
+      <Modal onClose={this.props.onClose}>
+
+      </Modal>
+    )
+  }
+}
