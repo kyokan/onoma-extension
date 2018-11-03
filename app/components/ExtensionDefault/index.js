a1d323e06d298d97e6ccef2af1f52d810f6f7587
sdtsui <szehungdanieltsui@gmail.com>
2018-11-03 19:25:35 +0100

    [WIP] Add ExtensionDefault-slider

--- app/components/ExtensionDefault/index.js
+++ app/components/ExtensionDefault/index.js
@@ -24,17 +24,26 @@ export default class App extends Component {
       <div>
         <div className='extension_primary_section'>
           <div className='header_text'>onoma wallet</div>
-          <div className='subheader_text'> Take control of your Handshake coins and domain names. </div>
-          <button className='extension_cta_button' onClick={() => {this.props.actions.setView('create-password')}}> Get Started </button>
+          <div className='subheader_text'>Take control of your Handshake coins and domain names.</div>
+          <button className='extension_cta_button' onClick={() => {this.props.actions.setView('create-password')}}>Get Started</button>
+          <div className='extension_primary_line_break'> </div>
         </div>
-        <div className='extension_secondary_section'>
-          <div onClick={actions.openExtensionInBrowser}> Browse Domains </div>
-          <div> Information </div>
-          <div></div>
-
-          <div> Resolve on Handshake </div>
-          <div> Version 1.0 </div>
-          <div> Current Height: </div>
+        <div className='extension_primary_section'>
+          <div className='extension_option_text' onClick={actions.openExtensionInBrowser}> Browse Domains </div>
+          <div className='extension_option_text'> Information </div>
+          <div className='extension_option_text'> Help </div>
+          <div>
+            <span>
+              <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round"></span>
+              </label>
+            </span>
+            <span>Resolve on Handshake</span>
+          </div>
+          <div className='extension_backgroud_text'>Version 1.0</div>
+          <div className='extension_backgroud_text'>Current Height: #3952</div>
+          <div className='extension_backgroud_text'>Hash: 0fj48fj30fuw-0fj48fj30fuw-0fj48fj30fuw</div>
         </div>
 
       </div>
