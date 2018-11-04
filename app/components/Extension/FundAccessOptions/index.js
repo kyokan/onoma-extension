f2dad6da7b1edb7a69578b5f588e8d0d2407e992
Daniel Tsui <szehungdanieltsui@gmail.com>
2018-11-04 09:45:26 +0100

    Dan (#38)
    * Save package-lock
    
    * Fix remaining PropTypes imports from @chikeichan upgrade to react 16
    
    * Add typeface -  SF-Pro-Display
    
    * [WIP] ExtensionDefault component styles
    
    * [WIP] Add ExtensionDefault-slider
    
    * [WIP] Style Resolver Slider
    
    * Revert to Roboto, Implement Extension Default Screen
    
    * Add cursor:pointer to supporting text
    
    * Separate components/css into default and general extensions
    
    * [WIP] Extension Password and FundAccessOption components
    
    * [WIP] Adjust FundAccess styles
    
    * Delete ExtensionDefault now that Extension/Default exists
    
    * Finalize at dimensions: 320x430  and padding: 1.2rem 1.4rem
    
    * [WIP] Styles for password input
    
    * [WIP] Styles for Select Access Options
    
    * Style select Fund Access Options
    
    * Add Ledger Status Indicators

--- app/components/Extension/FundAccessOptions/index.js
+++ app/components/Extension/FundAccessOptions/index.js
@@ -18,7 +18,44 @@ export default class FundAccessOptions extends Component {
 
   static propTypes = {};
 
+  state = {
+    activeButton: 'none',
+  };
+
+  handleClick = (optionName) => {
+    if (this.state.activeButton === optionName) {
+      this.props.actions.setView('default');
+    }
+
+    this.setState({ activeButton: optionName });
+  }
+
+  renderCheck = () => {
+    return (
+      <div className='circle-check-container'>
+        <div className='circle-check-symbol'> </div>
+      </div>
+    );
+  }
+
   render() {
+    let ledgerCheck = null;
+    let seedCheck = null;
+    let createCheck = null;
+
+    switch(this.state.activeButton) {
+      case 'ledger':
+        ledgerCheck = this.renderCheck();
+        break;
+      case 'seed':
+        seedCheck = this.renderCheck();
+        break;
+      case 'create':
+        createCheck = this.renderCheck();
+        break;
+      default:
+        break;
+    }
 
     return (
       <div className='extension_primary_section'>
@@ -33,18 +70,27 @@ export default class FundAccessOptions extends Component {
         <div className='extension_primary_line_break access_line_break'> </div>
           <div className='header_text'> How would you like to access your wallet? </div>
         <div className='access_cta_wrapper'>
-          <button className='access_cta_button' onClick={() => {}}>
-            <div className='access_cta_primary_text'> Connect Ledger Device </div>
+          <button className='access_cta_button' onClick={this.handleClick.bind(this, 'ledger')}>
+            {ledgerCheck}
+            <div className='access_cta_primary_text'>
+              <span>Connect Ledger Device</span>
+            </div>
             <div className='access_cta_secondary_text'> Recommended </div>
           </button>
 
-          <button className='access_cta_button' onClick={() => {}}>
-            <div className='access_cta_primary_text'> Import Seed Phrase </div>
+          <button className='access_cta_button' onClick={this.handleClick.bind(this, 'seed')}>
+            {seedCheck}
+            <div className='access_cta_primary_text'>
+              <span>Import Seed Phrase</span>
+            </div>
             <div className='access_cta_secondary_text'> Not Secure </div>
           </button>
 
-          <button className='access_cta_button' onClick={() => {}}>
-            <div className='access_cta_primary_text'> Create A New Wallet </div>
+          <button className='access_cta_button' onClick={this.handleClick.bind(this, 'create')}>
+            {createCheck}
+            <div className='access_cta_primary_text'>
+              <span> Create A New Wallet </span>
+            </div>
             <div className='access_cta_secondary_text'> Not Secure </div>
           </button>
         </div>
