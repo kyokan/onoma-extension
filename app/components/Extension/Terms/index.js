26cca0a5e1979dcecd1aaaf11f951dc285056205
sdtsui <szehungdanieltsui@gmail.com>
2018-11-12 15:32:12 -0800

    Update copy for terms

--- app/components/Extension/Terms/index.js
+++ app/components/Extension/Terms/index.js
@@ -32,15 +32,16 @@ export default class CreatePassword extends Component {
         </div>
         <div className='extension_primary_line_break terms_line_break'> </div>
         <div className='header_text'> Terms of Use </div>
-        <div className='subheader_text terms_subheader'> Please review and agree to the Handshake wallet's terms of use.</div>
+        <div className='subheader_text terms_subheader'> Please carefully review and agree to the Terms of Use.</div>
         <button className='terms_button clickable'>
           <span> Terms of Use </span>
+        {/* Default: Link out to external resource. Need to Chat with team regarding branding, where to host these terms. */}
           <span className='directional_symbol terms_forward_arrow'>
             <i className="right"></i>
           </span>
         </button>
 
-        <button className='extension_cta_button terms_cta' onClick={() => {this.props.actions.setView('default')}}> Accept </button>
+        <button className='extension_cta_button terms_cta' onClick={() => {this.props.actions.setView('default')}}> I Agree </button>
       </div>
     );
   }
--- app/pages/App.js
+++ app/pages/App.js
@@ -35,7 +35,7 @@ export default class App extends Component {
 
 
     return (
-      <ImportSeedWarning />
+      <ExtensionTerms />
     );
 
 
