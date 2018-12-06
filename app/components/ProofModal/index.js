6028c63c3f63a90d0610253182db7b6a7b7cfe25
Makzent <arnold.michael91@gmail.com>
2018-12-06 18:30:21 +0100

    refactored proof modal according to new design

--- app/components/ProofModal/index.js
+++ app/components/ProofModal/index.js
@@ -3,23 +3,27 @@ import PropTypes from 'prop-types';
 import c from 'classnames';
 import Modal from '../Modal';
 import './proof-modal.scss';
-import copy from "copy-to-clipboard";
+import copy from 'copy-to-clipboard';
 
 export default class ProofModal extends Component {
   static propTypes = {
     address: PropTypes.string.isRequired,
     stepOneInstruction: PropTypes.string.isRequired,
     stepTwoInstruction: PropTypes.string.isRequired,
+    stepThreeInstruction: PropTypes.string.isRequired,
+    stepOneDescription: PropTypes.string.isRequired,
+    stepTwoDescription: PropTypes.string.isRequired,
+    stepThreeDescription: PropTypes.string.isRequired,
     onSubmit: PropTypes.func.isRequired,
-    onClose: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired
   };
 
   static defaultProps = {
-    address: '3P3QsMVK89JBNqZQv5zMAKG8FK3kJM4rjt',
+    address: '3P3QsMVK89JBNqZQv5zMAKG8FK3kJM4rjt'
   };
 
   state = {
-    hasCopied: false,
+    hasCopied: false
   };
 
   copyAddress = () => {
@@ -29,15 +33,27 @@ export default class ProofModal extends Component {
   };
 
   render() {
-    const { onClose, address, stepOneInstruction, stepTwoInstruction, onSubmit } = this.props;
+    const {
+      onClose,
+      address,
+      stepOneInstruction,
+      stepTwoInstruction,
+      stepThreeInstruction,
+      stepOneDescription,
+      stepTwoDescription,
+      stepThreeDescription,
+      onSubmit
+    } = this.props;
     const { hasCopied } = this.state;
 
     return (
       <Modal className="proof" onClose={onClose}>
         <div className="proof__container">
           <div className="proof__header">
-            <div className="proof__title">Claim your coins with a proof</div>
-            <div className="proof__close-btn" onClick={onClose}>✕</div>
+            <div className="proof__title">Submit your SSH proof</div>
+            <div className="proof__close-btn" onClick={onClose}>
+              ✕
+            </div>
           </div>
           <div className="proof__content">
             <div className="proof__step">
@@ -45,11 +61,19 @@ export default class ProofModal extends Component {
                 <span className="proof__step-title--bolded">Step 1:</span>
                 <span>{stepOneInstruction}</span>
               </div>
+              <div className="proof__step-description">{stepOneDescription}</div>
+            </div>
+            <div className="proof__step">
+              <div className="proof__step-title">
+                <span className="proof__step-title--bolded">Step 2:</span>
+                <span>{stepTwoInstruction}</span>
+              </div>
+              <div className="proof__step-description">{stepTwoDescription}</div>
               <div className="proof__address-display">
                 <div className="proof__address">{address}</div>
                 <button
                   className={c('proof__copy-btn', {
-                    'proof__copy-btn--copied': hasCopied,
+                    'proof__copy-btn--copied': hasCopied
                   })}
                   onClick={this.copyAddress}
                 >
@@ -59,16 +83,22 @@ export default class ProofModal extends Component {
             </div>
             <div className="proof__step">
               <div className="proof__step-title">
-                <span className="proof__step-title--bolded">Step 2:</span>
-                <span>{stepTwoInstruction}</span>
+                <span className="proof__step-title--bolded">Step 3:</span>
+                <span>{stepThreeInstruction}</span>
               </div>
+              <div className="proof__step-description">{stepThreeDescription}</div>
               <div className="proof__paste-area">
                 <textarea placeholder="Paste proof content here" />
               </div>
             </div>
           </div>
           <div className="proof__footer">
-            <button className="proof__submit-btn" onClick={onSubmit}>Add Proof</button>
+            <button className="proof__details-btn" onClick={() => console.log('show details')}>
+              See full details
+            </button>
+            <button className="proof__submit-btn" onClick={onSubmit}>
+              Add Proof
+            </button>
           </div>
         </div>
       </Modal>
