9b8971b55e158198bd2cfa33e9b9676583487d6c
Michael Arnold <arnold.michael91@gmail.com>
2018-12-11 16:42:47 +0100

    Add Mocked Transactions UI; New Claim Coin modal
    * initial implementation of tx list view
    
    * added mediaqueries
    
    * implemented variables and added classname styling
    
    * added hover and active effect for rows
    
    * changed hover and active effect
    
    * switched opacity to different font color
    
    * refactored proof modal according to new design
    
    * changed padding of buttons
    
    * improved tx styling
    
    * added timestampConverter util function
    
    * refactoring transactions into its own component step 1
    
    * refactoring transactions into its own component pt.2
    
    * transactions refactoring pt 3
    
    * refactoring transactions pt4
    
    * cleanup after refactoring transactions component
    
    * added back proptype
    
    * cleanup
    
    * implemented request PR changes

--- app/pages/Window/Account/index.js
+++ app/pages/Window/Account/index.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { BigNumber as bn } from 'bignumber.js';
+
 import Modal from '../../../components/Modal/index';
 import './account.scss';
+import Transactions from '../../../components/Transactions';
+
+// Dummy transactions state until we have ducks
+import { transactionsDummyOrder } from '../../../utils/mockingTransactionsState';
 
 class Account extends Component {
   static propTypes = {
     accountBase: PropTypes.string.isRequired,
     accountIndex: PropTypes.number.isRequired,
     balance: PropTypes.object.isRequired,
-    transactions: PropTypes.array.isRequired,
-    domains: PropTypes.array.isRequired,
+    domains: PropTypes.array.isRequired
   };
 
   static defaultProps = {
@@ -19,11 +23,11 @@ class Account extends Component {
     accountIndex: 0,
     balance: bn(0),
     transactions: [],
-    domains: [],
+    domains: []
   };
 
   state = {
-    isShowingAccountModal: false,
+    isShowingAccountModal: false
   };
 
   openModal = () => this.setState({ isShowingAccountModal: true });
@@ -32,11 +36,11 @@ class Account extends Component {
   renderEmpty = text => <div className="account__empty-list">{text}</div>;
 
   renderTransactions() {
-    const { transactions } = this.props;
-
-    return !transactions.length
-      ? this.renderEmpty('You do not have any transactions')
-      : transactions.map(tx => <div>I am a transaction</div>);
+    return !transactionsDummyOrder.length ? (
+      this.renderEmpty('You do not have any transactions')
+    ) : (
+      <Transactions />
+    );
   }
 
   renderDomains() {
@@ -44,53 +48,39 @@ class Account extends Component {
 
     return !domains.length
       ? this.renderEmpty('You have no domains.js')
-      : domains.map(tx => <div>I am a domain</div>);
+      : domains.map(() => <div>I am a domain</div>);
   }
 
   renderAccountModal() {
     const { accountBase, accountIndex } = this.props;
 
-    return !this.state.isShowingAccountModal
-      ? null
-      : (
-        <Modal
-          className="account__switch-account-modal"
-          onClose={this.closeModal}
-        >
-          <div className="account__switch-account-modal__wrapper">
-            <div
-              className="account__switch-account-modal__close-btn"
-              onClick={this.closeModal}
-            >
-              ✕
-            </div>
-            <div className="account__switch-account-modal__header">
-              <div className="account__switch-account-modal__title">
-                Switch Account
-              </div>
-              <div className="account__switch-account-modal__subtitle">
-                Enter an account index you would like to interact with
-              </div>
-            </div>
-            <div className="account__switch-account-modal__content">
-              <div className="account__switch-account-modal__account-base">
-                {`${accountBase}`}
-              </div>
-              <input
-                type="number"
-                className="account__switch-account-modal__account-index"
-                placeholder={accountIndex}
-                min="0"
-              />
-            </div>
-            <div className="account__switch-account-modal__footer">
-              <button className="account__switch-account-modal__btn">
-                Switch
-              </button>
+    return !this.state.isShowingAccountModal ? null : (
+      <Modal className="account__switch-account-modal" onClose={this.closeModal}>
+        <div className="account__switch-account-modal__wrapper">
+          <div className="account__switch-account-modal__close-btn" onClick={this.closeModal}>
+            ✕
+          </div>
+          <div className="account__switch-account-modal__header">
+            <div className="account__switch-account-modal__title">Switch Account</div>
+            <div className="account__switch-account-modal__subtitle">
+              Enter an account index you would like to interact with
             </div>
           </div>
-        </Modal>
-      );
+          <div className="account__switch-account-modal__content">
+            <div className="account__switch-account-modal__account-base">{`${accountBase}`}</div>
+            <input
+              type="number"
+              className="account__switch-account-modal__account-index"
+              placeholder={accountIndex}
+              min="0"
+            />
+          </div>
+          <div className="account__switch-account-modal__footer">
+            <button className="account__switch-account-modal__btn">Switch</button>
+          </div>
+        </div>
+      </Modal>
+    );
   }
 
   render() {
@@ -98,37 +88,31 @@ class Account extends Component {
 
     return (
       <div className="account">
-        <div
-          className="account__address"
-        >
+        <div className="account__address">
           <div>Default Account</div>
           <div className="account__info-icon" />
         </div>
         <div className="account__header">
           <div className="account__balance-wrapper">
-            <div className="account__balance-wrapper__amount">
-              {`HNS ${+balance.toFixed(5)}`}
-            </div>
+            <div className="account__balance-wrapper__amount">{`HNS ${+balance.toFixed(5)}`}</div>
           </div>
         </div>
         <div className="account__content">
           <div className="account__transactions">
             <div className="account__panel-title">Your Recent Transactions</div>
-            { this.renderTransactions() }
+            {this.renderTransactions()}
           </div>
           <div className="account__domains">
             <div className="account__panel-title">Your Domains</div>
-            { this.renderDomains() }
+            {this.renderDomains()}
           </div>
         </div>
         {this.renderAccountModal()}
       </div>
-    )
+    );
   }
 }
 
-export default connect(
-  state => ({
-    balance: bn(state.wallet.balance.confirmed),
-  }),
-)(Account);
+export default connect(state => ({
+  balance: bn(state.wallet.balance.confirmed)
+}))(Account);
