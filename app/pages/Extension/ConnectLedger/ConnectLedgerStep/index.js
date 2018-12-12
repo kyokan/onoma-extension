d10ac55a35dd6c6c12fc154c53bc14767f795d98
Makzent <arnold.michael91@gmail.com>
2018-12-12 17:02:43 +0100

    cleanup and refactoring scss

--- app/pages/Extension/ConnectLedger/ConnectLedgerStep/index.js
+++ app/pages/Extension/ConnectLedger/ConnectLedgerStep/index.js
@@ -12,10 +12,10 @@ export default class ConnectLedgerStep extends Component {
   render() {
     const { stepNumber, stepDescription, stepCompleted } = this.props;
     return (
-      <div className="connect_status_pill">
-        <span className="connect_status_number">{stepNumber}</span>
-        <span className="connect_status_text">{stepDescription}</span>
-        <span className="connect_status_symbol">
+      <div className="connect__status-pill">
+        <span className="connect__status-number">{stepNumber}</span>
+        <span className="connect__status-text">{stepDescription}</span>
+        <span className="connect__status-symbol">
           <div
             className={classNames('ledger-circle-check-container', {
               'ledger-circle-check-container__active': stepCompleted,
