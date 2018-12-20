93b4517e9211ef649fcab2238af64facf9fd7686
Chi Kei Chan <chikeichan@gmail.com>
2018-12-20 15:12:22 +0800

    Add new home view

---
+++ app/pages/Extension/Home/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import c from 'classnames';
+import Hash from '../../../components/Hash';
+import './home.scss';
+
+@connect(
+  state => ({
+    height: state.chain.height,
+    currentHash: state.chain.currentHash,
+  }),
+)
+class Home extends Component {
+  static propTypes = {
+    currentHash: PropTypes.string.isRequired,
+    height: PropTypes.number.isRequired,
+  };
+
+  state = {
+    isToggledOn: false,
+  };
+
+  toggle = () => this.setState({ isToggledOn: !this.state.isToggledOn });
+
+  render() {
+    const { currentHash, height } = this.props;
+    return (
+      <div className="home">
+        <div className="home__header" />
+        <div className="home__content">
+          <div
+            className={c('home__content__power', {
+              'home__content__power--on': this.state.isToggledOn,
+            })}
+            onClick={this.toggle}
+          >
+            <div className="home__content__power__icon" />
+          </div>
+        </div>
+        <div className="home__footer">
+          <div>
+            {`Current Height: #${height}`}
+          </div>
+          <div>
+            Current Hash: <Hash value={currentHash} />
+          </div>
+        </div>
+      </div>
+    );
+  };
+}
+
+export default Home;
